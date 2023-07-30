71ca8e06f1e8726bbd9bc29be506d630b7f88d4d
BaKYCoder <bakycoder@gmail.com>
2023-07-31 01:37:00 +0200

    Register item in ModItems class.

--- src/client/java/com/baky/backtobed/item/ModItems.java
+++ src/client/java/com/baky/backtobed/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.baky.backtobed.item;
 
 import com.baky.backtobed.BackToBed;
+import com.baky.backtobed.item.custom.MagicalReturner;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -9,7 +10,7 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item MAGICAL_RETURNER = registerItem("magical_returner",
-            new Item(new FabricItemSettings().group(ItemGroup.TOOLS)));
+            new MagicalReturner(new FabricItemSettings().group(ItemGroup.TOOLS)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(BackToBed.MOD_ID, name), item);
