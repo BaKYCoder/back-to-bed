120377c2a6ecae48f7440bc8ce61ca0924dbbd50
BaKYCoder <bakycoder@gmail.com>
2023-10-07 18:08:18 +0200

    Initialize `ModItems` class.

--- fabric/src/main/java/com/baky/backtobed/fabric/BackToBedFabric.java
+++ fabric/src/main/java/com/baky/backtobed/fabric/BackToBedFabric.java
@@ -1,11 +1,13 @@
 package com.baky.backtobed.fabric;
 
 import com.baky.backtobed.BackToBed;
+import com.baky.backtobed.fabric.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class BackToBedFabric implements ModInitializer {
     @Override
     public void onInitialize() {
+        ModItems.init();
         BackToBed.init();
     }
 }
