635b981288396996d635ec8e0792503f6071ffb0
BaKYCoder <bakycoder@gmail.com>
2023-10-07 21:01:40 +0200

    Update to `1.17.1` version.

--- forge/src/main/java/com/baky/backtobed/forge/BackToBedForge.java
+++ forge/src/main/java/com/baky/backtobed/forge/BackToBedForge.java
@@ -1,12 +1,18 @@
 package com.baky.backtobed.forge;
 
 import com.baky.backtobed.BackToBed;
+import com.baky.backtobed.forge.item.ModItems;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BackToBed.MOD_ID)
 public class BackToBedForge {
     public BackToBedForge() {
+        ModItems.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+        MinecraftForge.EVENT_BUS.register(this);
+
         BackToBed.init();
     }
 }
