fc5254d51822e28947fe08a563c0434c309e0218
BaKYCoder <bakycoder@gmail.com>
2024-12-28 18:38:55 +0100

    Add class for registration of all items

---
+++ common/src/main/java/com/github/bakycoder/backtobed/AllItems.java
@@ -0,0 +1,17 @@
+package com.github.bakycoder.backtobed;
+
+import com.github.bakycoder.backtobed.item.MagicalReturner;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+
+public class AllItems {
+    public static final Item MAGICAL_RETURNER = register("magical_returner", new MagicalReturner(new Item.Properties().stacksTo(1)));
+
+    private static <T extends Item> T register(String path, T item) {
+        return Registry.register(BuiltInRegistries.ITEM, ResourceLocation.tryBuild(Constants.MOD_ID, path), item);
+    }
+
+    public static void register() {}
+}
