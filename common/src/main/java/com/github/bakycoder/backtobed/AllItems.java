19e8b62c8715a9f12c23b97aa0b993002ecc3ccb
BaKYCoder <bakycoder@gmail.com>
2025-01-03 17:27:42 +0100

    Create returner instances using a common `Returner` class

--- common/src/main/java/com/github/bakycoder/backtobed/AllItems.java
+++ common/src/main/java/com/github/bakycoder/backtobed/AllItems.java
@@ -1,26 +1,16 @@
 package com.github.bakycoder.backtobed;
 
 import com.github.bakycoder.backtobed.item.HellsReturner;
-import com.github.bakycoder.backtobed.item.MagicalReturner;
+import com.github.bakycoder.backtobed.item.Returner;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
 
 public class AllItems {
-    public static final Item MAGICAL_RETURNER = register("magical_returner",
-            new MagicalReturner(new Item
-                    .Properties()
-                    .stacksTo(1)
-            )
-    );
-
-    public static final Item HELLS_RETURNER = register("hells_returner",
-            new HellsReturner(new Item
-                    .Properties()
-                    .stacksTo(1)
-            )
-    );
+    public static final Item MAGICAL_RETURNER = register("magical_returner", new Returner(Level.OVERWORLD));
+    public static final Item HELLS_RETURNER = register("hells_returner", new Returner(Level.NETHER));
 
     private static <T extends Item> T register(String path, T item) {
         return Registry.register(BuiltInRegistries.ITEM, ResourceLocation.tryBuild(Constants.MOD_ID, path), item);
