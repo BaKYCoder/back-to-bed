d95e2e3ba15caf70913d87a2530ff4a843bc58bf
BaKYCoder <bakycoder@gmail.com>
2025-01-04 19:58:59 +0100

    Replace item registration with service registry

--- common/src/main/java/com/github/bakycoder/backtobed/AllItems.java
+++ common/src/main/java/com/github/bakycoder/backtobed/AllItems.java
@@ -1,20 +1,17 @@
 package com.github.bakycoder.backtobed;
 
-import com.github.bakycoder.backtobed.item.HellsReturner;
 import com.github.bakycoder.backtobed.item.Returner;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
+import com.github.bakycoder.backtobed.platform.Services;
+
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 
 public class AllItems {
-    public static final Item MAGICAL_RETURNER = register("magical_returner", new Returner(Level.OVERWORLD));
-    public static final Item HELLS_RETURNER = register("hells_returner", new Returner(Level.NETHER));
+    public static final Item MAGICAL_RETURNER = Services.ITEM_REGISTRY
+            .register("magical_returner", new Returner(Level.OVERWORLD));
 
-    private static <T extends Item> T register(String path, T item) {
-        return Registry.register(BuiltInRegistries.ITEM, ResourceLocation.tryBuild(Constants.MOD_ID, path), item);
-    }
+    public static final Item HELLS_RETURNER = Services.ITEM_REGISTRY
+            .register("hells_returner", new Returner(Level.NETHER));
 
     public static void register() {}
 }
