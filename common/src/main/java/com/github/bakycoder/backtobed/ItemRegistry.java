f0cc8cfab2cba98db44f170c10687236f88e6506
BaKYCoder <bakycoder@gmail.com>
2025-01-11 19:01:20 +0100

    Save instance of registered items

--- common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
+++ common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
@@ -1,24 +1,16 @@
 package com.github.bakycoder.backtobed;
 
-import com.github.bakycoder.backtobed.util.ItemHolder;
 import com.github.bakycoder.backtobed.item.returners.Returner;
 import com.github.bakycoder.backtobed.platform.Services;
 import com.github.bakycoder.backtobed.platform.services.IItemRegistrar;
 import net.minecraft.world.level.Level;
 
-import java.util.List;
-
 public class ItemRegistry {
     private static final IItemRegistrar REGISTRAR = Services.getItemRegistrar();
 
-    public static final List<ItemHolder> RETURNERS = List.of(
-            new ItemHolder("magical_returner", () -> new Returner(Level.OVERWORLD)),
-            new ItemHolder("hells_returner", () -> new Returner(Level.NETHER))
-    );
+    public static final Object
+            MAGICAL_RETURNER = REGISTRAR.register("magical_returner", () -> new Returner(Level.OVERWORLD)),
+            HELLS_RETURNER = REGISTRAR.register("hells_returner", () -> new Returner(Level.NETHER));
 
-    public static void registerItems() {
-        for (ItemHolder holder : RETURNERS) {
-            REGISTRAR.register(holder.path(), holder.item());
-        }
-    }
+    public static void registerItems() {}
 }
