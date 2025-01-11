e56de3d878edc9fbdd3a2a6c7b11700a7fa69ddc
BaKYCoder <bakycoder@gmail.com>
2025-01-11 19:02:28 +0100

    Remove record that will no longer be used

--- common/src/main/java/com/github/bakycoder/backtobed/util/ItemHolder.java
+++
@@ -1,8 +0,0 @@
-package com.github.bakycoder.backtobed.util;
-
-import net.minecraft.world.item.Item;
-
-import java.util.function.Supplier;
-
-public record ItemHolder(String path, Supplier<Item> item) {
-}
