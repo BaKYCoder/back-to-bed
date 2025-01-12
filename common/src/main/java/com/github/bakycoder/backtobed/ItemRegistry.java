c407db44eb884e3661f039d40e092c79bceb0fd9
BaKYCoder <bakycoder@gmail.com>
2025-01-12 14:11:44 +0100

    Replace `Object` type with nested interface in `IItemRegistrar`

--- common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
+++ common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
@@ -3,12 +3,13 @@ package com.github.bakycoder.backtobed;
 import com.github.bakycoder.backtobed.item.returners.Returner;
 import com.github.bakycoder.backtobed.platform.Services;
 import com.github.bakycoder.backtobed.platform.services.IItemRegistrar;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 
 public class ItemRegistry {
     private static final IItemRegistrar REGISTRAR = Services.getItemRegistrar();
 
-    public static final Object
+    public static final IItemRegistrar.IRegisteredItem<Item>
             MAGICAL_RETURNER = REGISTRAR.register("magical_returner", () -> new Returner(Level.OVERWORLD)),
             HELLS_RETURNER = REGISTRAR.register("hells_returner", () -> new Returner(Level.NETHER));
 
