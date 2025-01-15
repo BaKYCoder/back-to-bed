269e23a859cb21d98f04d7c9f3bcf329616b4f14
BaKYCoder <bakycoder@gmail.com>
2025-01-15 20:11:06 +0100

    Add effect classes for each returner to handle teleportation effects

--- common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
+++ common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
@@ -1,5 +1,7 @@
 package com.github.bakycoder.backtobed;
 
+import com.github.bakycoder.backtobed.item.returners.effects.HellsEffectProvider;
+import com.github.bakycoder.backtobed.item.returners.effects.MagicalEffectProvider;
 import com.github.bakycoder.backtobed.item.returners.Returner;
 import com.github.bakycoder.backtobed.platform.Services;
 import com.github.bakycoder.backtobed.platform.services.IItemRegistrar;
@@ -11,8 +13,8 @@ public class ItemRegistry {
     private static final IItemRegistrar REGISTRAR = Services.getItemRegistrar();
 
     public static final IItemRegistrar.IRegisteredItem<Item>
-            MAGICAL_RETURNER = REGISTRAR.register("magical_returner", () -> new Returner(Level.OVERWORLD)),
-            HELLS_RETURNER = REGISTRAR.register("hells_returner", () -> new Returner(Level.NETHER));
+            MAGICAL_RETURNER = REGISTRAR.register("magical_returner", () -> new Returner(Level.OVERWORLD, MagicalEffectProvider::new)),
+            HELLS_RETURNER = REGISTRAR.register("hells_returner", () -> new Returner(Level.NETHER, HellsEffectProvider::new));
 
     public static void acceptItemsToTab(CreativeModeTab.Output output) {
         output.accept(MAGICAL_RETURNER.get());
