ab6d7c1ffd1b77d9b89cdcee5c767e451d019b53
BaKYCoder <bakycoder@gmail.com>
2025-01-15 20:09:41 +0100

    Implement effect provider interface for magical returner

---
+++ common/src/main/java/com/github/bakycoder/backtobed/item/returners/effects/MagicalEffectProvider.java
@@ -0,0 +1,29 @@
+package com.github.bakycoder.backtobed.item.returners.effects;
+
+import com.github.bakycoder.backtobed.api.SoundEffect;
+import com.github.bakycoder.backtobed.api.provider.IEffectProvider;
+import net.minecraft.core.particles.DustColorTransitionOptions;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.sounds.SoundEvents;
+
+import org.joml.Vector3f;
+
+import java.util.List;
+
+public class MagicalEffectProvider implements IEffectProvider {
+    @Override
+    public ParticleOptions getParticles() {
+        return new DustColorTransitionOptions(
+                new Vector3f(0.0F, 1.0F, 1.0F),
+                new Vector3f(1.0F, 0.0F, 1.0F),
+                1f
+        );
+    }
+
+    @Override
+    public List<SoundEffect> getSounds() {
+        return List.of(
+                new SoundEffect(SoundEvents.AMETHYST_CLUSTER_HIT, 1.0F, 1.0F)
+        );
+    }
+}
