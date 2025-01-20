ff9007d07e50e9449ebc05cd7056da14fac8b68f
BaKYCoder <bakycoder@gmail.com>
2025-01-20 19:29:27 +0100

    Remove `isEnabled` method, add new one for handling teleportation

--- common/src/main/java/com/github/bakycoder/backtobed/api/provider/IFeatureInjector.java
+++ common/src/main/java/com/github/bakycoder/backtobed/api/provider/IFeatureInjector.java
@@ -1,11 +1,11 @@
 package com.github.bakycoder.backtobed.api.provider;
 
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
 public interface IFeatureInjector {
-    boolean isEnabled();
-    void inject(Level level, LivingEntity entity, ItemStack stack, Vec3 destination);
+    boolean handlesTeleportation();
+    void inject(ServerPlayer player, ServerLevel respawn, ItemStack stack, Vec3 destination);
 }
