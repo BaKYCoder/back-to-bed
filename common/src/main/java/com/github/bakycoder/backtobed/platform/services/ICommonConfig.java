1ddbc06e134bae7f2631b32b22d4f6da2b519e6c
BaKYCoder <bakycoder@gmail.com>
2025-01-21 21:36:59 +0100

    Delete interface for configuration managing and replace with new one

--- common/src/main/java/com/github/bakycoder/backtobed/platform/services/ICommonConfig.java
+++
@@ -1,6 +0,0 @@
-package com.github.bakycoder.backtobed.platform.services;
-
-public interface ICommonConfig {
-    int getReturnerDurationUsage();
-    int getReturnerCooldown();
-}
---
+++ common/src/main/java/com/github/bakycoder/backtobed/platform/services/IModConfig.java
@@ -0,0 +1,6 @@
+package com.github.bakycoder.backtobed.platform.services;
+
+public interface IModConfig {
+    int getDefaultReturnerDurationUsage();
+    int getDefaultReturnerCooldown();
+}
