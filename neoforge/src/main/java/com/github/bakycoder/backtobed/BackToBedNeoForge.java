8bfb3fb7f3c5f9ed41a313a4f7641a7569c887a5
BaKYCoder <bakycoder@gmail.com>
2025-01-21 21:40:19 +0100

    Register config class in NeoForge main class

--- neoforge/src/main/java/com/github/bakycoder/backtobed/BackToBedNeoForge.java
+++ neoforge/src/main/java/com/github/bakycoder/backtobed/BackToBedNeoForge.java
@@ -1,24 +1,28 @@
 package com.github.bakycoder.backtobed;
 
 
+import com.github.bakycoder.backtobed.platform.NeoForgeModConfig;
 import com.github.bakycoder.backtobed.platform.NeoForgeItemRegistrar;
 
 import net.minecraft.world.item.CreativeModeTabs;
 
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 @Mod(BackToBed.MOD_ID)
 public class BackToBedNeoForge {
-
-    public BackToBedNeoForge(IEventBus eBus) {
+    public BackToBedNeoForge(IEventBus modEventBus, ModContainer modContainer) {
         BackToBed.LOGGER.info("Hello NeoForge world!");
 
         BackToBed.initCommon();
 
-        NeoForgeItemRegistrar.registerToEventBus(eBus);
-        eBus.addListener(this::configureTabEntries);
+        NeoForgeItemRegistrar.registerToEventBus(modEventBus);
+        modEventBus.addListener(this::configureTabEntries);
+
+        modContainer.registerConfig(ModConfig.Type.SERVER, NeoForgeModConfig.SPEC);
     }
 
     private void configureTabEntries(BuildCreativeModeTabContentsEvent event) {
