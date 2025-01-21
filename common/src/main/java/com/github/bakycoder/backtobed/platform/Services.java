acc573dd83b36534bed2d3aa0f2f06a7d2ffa40b
BaKYCoder <bakycoder@gmail.com>
2025-01-21 21:37:56 +0100

    Load config as service

--- common/src/main/java/com/github/bakycoder/backtobed/platform/Services.java
+++ common/src/main/java/com/github/bakycoder/backtobed/platform/Services.java
@@ -1,6 +1,7 @@
 package com.github.bakycoder.backtobed.platform;
 
 import com.github.bakycoder.backtobed.BackToBed;
+import com.github.bakycoder.backtobed.platform.services.IModConfig;
 import com.github.bakycoder.backtobed.platform.services.IItemRegistrar;
 import com.github.bakycoder.backtobed.platform.services.IPlatformHelper;
 
@@ -9,6 +10,7 @@ import java.util.ServiceLoader;
 public class Services {
     private static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
     private static final IItemRegistrar ITEM_REGISTRAR = load(IItemRegistrar.class);
+    private static final IModConfig MOD_CONFIG = load(IModConfig.class);
 
     public static IPlatformHelper getPlatform() {
         return PLATFORM;
@@ -16,6 +18,10 @@ public class Services {
 
     public static IItemRegistrar getItemRegistrar() {
         return ITEM_REGISTRAR;
+    }
+
+    public static IModConfig getModConfig() {
+        return MOD_CONFIG;
     }
 
     public static <T> T load(Class<T> clazz) {
