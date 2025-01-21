9be6c01cd8b60f98676e3de5ab036c95e118a9a6
BaKYCoder <bakycoder@gmail.com>
2025-01-21 21:39:43 +0100

    Create class as service for NeoForge loader to manage configuration

---
+++ neoforge/src/main/java/com/github/bakycoder/backtobed/platform/NeoForgeModConfig.java
@@ -0,0 +1,43 @@
+package com.github.bakycoder.backtobed.platform;
+
+import com.github.bakycoder.backtobed.BackToBed;
+import com.github.bakycoder.backtobed.platform.services.IModConfig;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.config.ModConfigEvent;
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+@EventBusSubscriber(modid = BackToBed.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+public class NeoForgeModConfig implements IModConfig {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    private static final ModConfigSpec.ConfigValue<Integer> DEFAULT_RETURNER_DURATION_USAGE = BUILDER
+            .comment("Default duration usage of all returners (in ticks)")
+            .defineInRange("default_returner_duration_usage", 60, 0, 72000);
+
+    private static final ModConfigSpec.ConfigValue<Integer> DEFAULT_RETURNER_COOLDOWN = BUILDER
+            .comment("Default cooldown duration for all returners after usage (in ticks)")
+            .defineInRange("default_returner_cooldown", 50, 0, 72000);
+
+    public static final ModConfigSpec SPEC = BUILDER.build();
+
+    private static int defaultReturnerDurationUsage;
+    private static int defaultReturnerCooldown;
+
+    @Override
+    public int getDefaultReturnerDurationUsage() {
+        return defaultReturnerDurationUsage;
+    }
+
+    @Override
+    public int getDefaultReturnerCooldown() {
+        return defaultReturnerCooldown;
+    }
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+        defaultReturnerDurationUsage = DEFAULT_RETURNER_DURATION_USAGE.get();
+        defaultReturnerCooldown = DEFAULT_RETURNER_COOLDOWN.get();
+    }
+}
