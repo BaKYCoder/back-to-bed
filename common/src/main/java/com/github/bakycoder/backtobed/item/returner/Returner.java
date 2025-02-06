b6f1f6e2a4ccdd30b4f017067a285d455eab95a7
BaKYCoder <bakycoder@gmail.com>
2025-02-06 14:01:22 +0100

    Deal damage to Returner after teleportation

--- common/src/main/java/com/github/bakycoder/backtobed/item/returner/Returner.java
+++ common/src/main/java/com/github/bakycoder/backtobed/item/returner/Returner.java
@@ -18,10 +18,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.phys.Vec3;
@@ -93,6 +90,10 @@ public class Returner extends Item {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        if(player.getItemInHand(usedHand).getMaxDamage() - player.getItemInHand(usedHand).getDamageValue() <= 0) {
+            return InteractionResultHolder.pass(player.getItemInHand(usedHand));
+        }
+
         player.startUsingItem(usedHand);
         return InteractionResultHolder.consume(player.getItemInHand(usedHand));
     }
@@ -153,6 +154,8 @@ public class Returner extends Item {
         }
 
         EFFECT_PROVIDER.applyEffects(respawnLevel, player, destination);
+
+        stack.setDamageValue(stack.getDamageValue() + 1);
 
         player.stopUsingItem();
         player.getCooldowns().addCooldown(stack.getItem(), MOD_CONFIG.getReturnerCooldown());
