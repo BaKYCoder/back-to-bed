9e023cd22b8de6f15c491a7f8164e37049f64ba8
BaKYCoder <bakycoder@gmail.com>
2025-02-08 22:26:12 +0100

    Add color name for returners

--- common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
+++ common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
@@ -6,6 +6,7 @@ import com.github.bakycoder.backtobed.item.returner.Returner;
 import com.github.bakycoder.backtobed.item.returner.features.MountFeatureInjector;
 import com.github.bakycoder.backtobed.platform.Services;
 import com.github.bakycoder.backtobed.platform.services.IItemRegistrar;
+import net.minecraft.ChatFormatting;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
@@ -14,8 +15,8 @@ public class ItemRegistry {
     private static final IItemRegistrar REGISTRAR = Services.getItemRegistrar();
 
     public static final IItemRegistrar.IRegisteredItem<Item>
-            MAGICAL_RETURNER = REGISTRAR.register("magical_returner", () -> new Returner(Level.OVERWORLD, MagicalEffectProvider::new, MountFeatureInjector::new)),
-            HELLS_RETURNER = REGISTRAR.register("hells_returner", () -> new Returner(Level.NETHER, HellsEffectProvider::new));
+            MAGICAL_RETURNER = REGISTRAR.register("magical_returner", () -> new Returner(ChatFormatting.AQUA, Level.OVERWORLD, MagicalEffectProvider::new, MountFeatureInjector::new)),
+            HELLS_RETURNER = REGISTRAR.register("hells_returner", () -> new Returner(ChatFormatting.YELLOW, Level.NETHER, HellsEffectProvider::new));
 
     public static void acceptItemsToTab(CreativeModeTab.Output output) {
         output.accept(MAGICAL_RETURNER.get());
