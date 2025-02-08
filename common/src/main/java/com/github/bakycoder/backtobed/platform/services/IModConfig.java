73c760779f51874083b1695734fec688f73da1dd
BaKYCoder <bakycoder@gmail.com>
2025-02-08 22:25:52 +0100

    Add new method for configuration, add argument to methods

--- common/src/main/java/com/github/bakycoder/backtobed/platform/services/IModConfig.java
+++ common/src/main/java/com/github/bakycoder/backtobed/platform/services/IModConfig.java
@@ -1,6 +1,9 @@
 package com.github.bakycoder.backtobed.platform.services;
 
+import com.github.bakycoder.backtobed.item.returner.Returner;
+
 public interface IModConfig {
-    int getReturnerDurationUsage();
-    int getReturnerCooldown();
+    int getReturnerDurationUsage(Returner returner);
+    int getReturnerCooldown(Returner returner);
+    boolean showReturnerTooltip(Returner returner);
 }
