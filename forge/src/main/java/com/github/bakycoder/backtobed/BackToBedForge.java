8550199d918bfebf3b4b75a1fa4242d4c9f9dedf
BaKYCoder <bakycoder@gmail.com>
2025-04-24 23:04:21 +0200

    Register forge config from loading context

--- forge/src/main/java/com/github/bakycoder/backtobed/BackToBedForge.java
+++ forge/src/main/java/com/github/bakycoder/backtobed/BackToBedForge.java
@@ -2,11 +2,14 @@ package com.github.bakycoder.backtobed;
 
 import com.github.bakycoder.backtobed.platform.ForgeItemRegistrar;
 
+import com.github.bakycoder.backtobed.platform.ForgeModConfig;
 import net.minecraft.world.item.CreativeModeTabs;
 
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BackToBed.MOD_ID)
@@ -20,6 +23,8 @@ public class BackToBedForge {
 
         ForgeItemRegistrar.registerToEventBus(MOD_EVENT_BUS);
         MOD_EVENT_BUS.addListener(this::configureTabEntries);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ForgeModConfig.SPEC);
     }
 
     private void configureTabEntries(BuildCreativeModeTabContentsEvent event) {
