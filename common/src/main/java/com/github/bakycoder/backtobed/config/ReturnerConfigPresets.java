f829cee358a96acf528ffcf147a2868257328c64
BaKYCoder <bakycoder@gmail.com>
2025-04-27 00:28:02 +0200

    Add `ReturnerConfigPresets` class with predefined presets
    Introduced the ReturnerConfigPresets class to centralize preset configurations.

---
+++ common/src/main/java/com/github/bakycoder/backtobed/config/ReturnerConfigPresets.java
@@ -0,0 +1,9 @@
+package com.github.bakycoder.backtobed.config;
+
+public class ReturnerConfigPresets {
+    public static final Preset GLOBAL = new Preset("global", 60, 50, true);
+    public static final Preset MAGICAL = new Preset("magical_returner", -1, -1, true);
+    public static final Preset HELLS = new Preset("hells_returner" , 70, 60, true);
+
+    public record Preset(String name, int durationUsage, int cooldown, boolean showTooltip) {}
+}
