3f58e8d8f88077f831b1172eb119e2bd75aba990
BaKYCoder <bakycoder@gmail.com>
2025-04-28 23:44:10 +0200

    Add Mysterious Returner item to the ItemRegistry
    Introduced the Mysterious Returner with its associated effect provider. Updated the creative tab to include the new item and registered it for use in the END dimension.

--- common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
+++ common/src/main/java/com/github/bakycoder/backtobed/ItemRegistry.java
@@ -3,6 +3,7 @@ package com.github.bakycoder.backtobed;
 import com.github.bakycoder.backtobed.item.returner.effects.HellsEffectProvider;
 import com.github.bakycoder.backtobed.item.returner.effects.MagicalEffectProvider;
 import com.github.bakycoder.backtobed.item.returner.Returner;
+import com.github.bakycoder.backtobed.item.returner.effects.MysteriousEffectProvider;
 import com.github.bakycoder.backtobed.item.returner.features.MountFeatureInjector;
 import com.github.bakycoder.backtobed.platform.Services;
 import com.github.bakycoder.backtobed.platform.services.IItemRegistrar;
@@ -16,11 +17,13 @@ public class ItemRegistry {
 
     public static final IItemRegistrar.IRegisteredItem<Item>
             MAGICAL_RETURNER = REGISTRAR.register("magical_returner", () -> new Returner(ChatFormatting.AQUA, Level.OVERWORLD, MagicalEffectProvider::new, MountFeatureInjector::new)),
-            HELLS_RETURNER = REGISTRAR.register("hells_returner", () -> new Returner(ChatFormatting.YELLOW, Level.NETHER, HellsEffectProvider::new));
+            HELLS_RETURNER = REGISTRAR.register("hells_returner", () -> new Returner(ChatFormatting.YELLOW, Level.NETHER, HellsEffectProvider::new)),
+            MYSTERIOUS_RETURNER = REGISTRAR.register("mysterious_returner", () -> new Returner(ChatFormatting.LIGHT_PURPLE, Level.END, MysteriousEffectProvider::new));
 
     public static void acceptItemsToTab(CreativeModeTab.Output output) {
         output.accept(MAGICAL_RETURNER.get());
         output.accept(HELLS_RETURNER.get());
+        output.accept(MYSTERIOUS_RETURNER.get());
     }
 
     public static void registerItems() {
