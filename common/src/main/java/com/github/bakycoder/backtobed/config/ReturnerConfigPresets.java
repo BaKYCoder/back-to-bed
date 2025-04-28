f2975c97af35b50f5bc4a7f58679ed3830c3cc03
BaKYCoder <bakycoder@gmail.com>
2025-04-28 23:45:00 +0200

    Add MYSTERIOUS preset to ReturnerConfigPresets
    Introduce a new preset, "mysterious_returner" with specific duration and cooldown values.

--- common/src/main/java/com/github/bakycoder/backtobed/config/ReturnerConfigPresets.java
+++ common/src/main/java/com/github/bakycoder/backtobed/config/ReturnerConfigPresets.java
@@ -4,6 +4,7 @@ public class ReturnerConfigPresets {
     public static final Preset GLOBAL = new Preset("global", 60, 50, true);
     public static final Preset MAGICAL = new Preset("magical_returner", -1, -1, true);
     public static final Preset HELLS = new Preset("hells_returner" , 70, 60, true);
+    public static final Preset MYSTERIOUS = new Preset("mysterious_returner" , 90, 70, true);
 
     public record Preset(String name, int durationUsage, int cooldown, boolean showTooltip) {}
 }
